169ae8cb6a86a9b542ae8fed160d3e419ac9d0a7
nicle-lin <dghpgyss@163.com>
2017-02-09 14:30:09 +0800

    flag, unpack or not

--- lib/update/package.go
+++ lib/update/package.go
@@ -1,1 +1,13 @@
 package update
+
+var Flag uint16
+
+//return false,the caller have to unpack the SSU,and inc Flag
+func SetFlag()bool{
+	if Flag == 0 {
+		return false
+	}else{
+		Flag++
+		return true
+	}
+}

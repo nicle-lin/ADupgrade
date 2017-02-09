963b73307151d5e777c5cf5f124d6dc9e69ffad5
nicle-lin <dghpgyss@163.com>
2017-02-09 14:55:39 +0800

    flag, unpack or not

--- lib/update/package.go
+++ lib/update/package.go
@@ -3,11 +3,34 @@ package update
 var Flag uint16
 
 //return false,the caller have to unpack the SSU,and inc Flag
-func SetFlag()bool{
+func GetFlag()bool{
 	if Flag == 0 {
 		return false
 	}else{
-		Flag++
 		return true
 	}
+}
+//when unpack SSU done, it should call this function
+func IncFlag(){
+	Flag++
+}
+
+//when upgrade success, it should call this function
+func DecFlag(){
+	if Flag > 0{
+		Flag--
+	}
+}
+
+
+func UnpackSSU(){
+	if !GetFlag(){
+		IncFlag()
+		//don't have to unpack SSU,because it has been unpacked
+		return
+	}
+
+
+
+	IncFlag()
 }

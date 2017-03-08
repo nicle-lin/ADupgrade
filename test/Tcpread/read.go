851a3b69377ca6377388eac5d983d1382b3ab98c
nicle-lin <dghpgyss@163.com>
2017-03-08 23:57:49 +0800

    Get file

--- test/Tcpread/read.go
+++ test/Tcpread/read.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/nicle-lin/ADCM/lib/update"
-	"os"
+	//"os"
 	"fmt"
 )
 func main() {
-	if err := update.GetFile(os.Args[1],"admin","51111","ibdata1","/aclog/database/data/ibdata1");err != nil{
+	if err := update.GetFile("192.168.1.101","admin","51111","ibdata1","/aclog/database/data/ibdata1");err != nil{
+		fmt.Println(err)
+	}
+
+	if err := update.GetFile("192.168.1.101","admin","51111","","/aclog/database/data/mysql-bin.index");err != nil{
 		fmt.Println(err)
 	}
 }

a78bb4052e96bd601ec80a930e31a7641775fde8
nicle-lin <dghpgyss@163.com>
2017-04-17 23:06:06 +0800

    readframe , writeframe

--- test/mblb/mblb.go
+++ test/mblb/mblb.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"net"
-	"gopkg.in/fatih/pool.v2"
+
+	"fmt"
 )
-//frame:
+
+var (
+	network    = "tcp"
+	address    = "127.0.0.1:7777"
+	factory    = func() (net.Conn, error) { return net.Dial(network, address) }
+)
+
+
+
 
 
 func main() {
 
 	conn := net.DialTCP()
-	pool.Pool()
+}
+
+
+func client(){
+
+}
+
+func server(){
+	l, err := net.Listen(network, address)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		go func() {
+			buffer := make([]byte, 256)
+			conn.Read(buffer)
+		}()
+	}
 }

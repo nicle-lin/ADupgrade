23fa3538d77be7284db7780d2db09afb0e86a05c
nicle-lin <dghpgyss@163.com>
2017-04-18 23:28:17 +0800

    client,server

--- test/mblb/mblb.go
+++ test/mblb/mblb.go
@@ -4,29 +4,86 @@ import (
 	"net"
 
 	"fmt"
+	"github.com/nicle-lin/ADupgrade/test/mblb/proto"
+	"io"
 )
 
 var (
-	network    = "tcp"
-	address    = "127.0.0.1:7777"
-	factory    = func() (net.Conn, error) { return net.Dial(network, address) }
+	network = "tcp"
+	address = "127.0.0.1:7777"
+	factory = func() (net.Conn, error) { return net.Dial(network, address) }
 )
 
-
-
-
-
 func main() {
+	client()
+	server()
+}
+func handleClient() error {
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
 
-	conn := net.DialTCP()
+	for i := 0; i < 10; i++ {
+		_, err1 := proto.WriteFrame([]byte("hi,this is from client"), conn)
+		if err1 != nil {
+			return err1
+		}
+		_, err2 := proto.WriteFrame([]byte("how are you"), conn)
+		if err2 != nil {
+			return err2
+		}
+		_, err3 := proto.WriteFrame([]byte("what is your name"), conn)
+		if err3 != nil {
+			return err3
+		}
+	}
+	for {
+		_, err := proto.ReadFrame(conn)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
+func client() error {
 
-func client(){
-
+	for i := 0; i < 10; i++ {
+		go handleClient()
+	}
+	return nil
 }
 
-func server(){
+func handleServer(l net.Listener) error {
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	go func() {
+		for {
+			_, err := proto.ReadFrame(conn)
+			if err == io.EOF {
+				conn.Close()
+				break
+			} else if err != nil {
+				fmt.Println(err)
+			}
+			_, err1 := proto.WriteFrame([]byte("hi,this is from server"), conn)
+			if err1 != nil {
+				fmt.Println(err1)
+			}
+		}
+	}()
+	return nil
+}
+
+func server() {
 	l, err := net.Listen(network, address)
 	if err != nil {
 		fmt.Println(err)
@@ -34,14 +91,6 @@ func server(){
 	defer l.Close()
 
 	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		go func() {
-			buffer := make([]byte, 256)
-			conn.Read(buffer)
-		}()
+		go handleServer(l)
 	}
 }
--- test/mblb/proto/frame.go
+++ test/mblb/proto/frame.go
@@ -11,15 +11,15 @@ import (
  */
 
 const (
-	FRAME_HEADER_LEN   = 4      //a frame header is 4 bytes
-	FRAMEFLAG          = 0xf3db //a frame is started with "0xf3db"
-	MAX_FRAME_LEN      = 1020 + FRAME_HEADER_LEN
+	FRAME_HEADER_LEN = 4      //a frame header is 4 bytes
+	FRAMEFLAG        = 0xf3db //a frame is started with "0xf3db"
+	MAX_FRAME_LEN    = 1020 + FRAME_HEADER_LEN
 )
 
 type Frame interface {
-	ReadFrame(b []byte,conn net.Conn) (n int, err error)
+	ReadFrame(b []byte, conn net.Conn) (n int, err error)
 
-	WriteFrame(b []byte,conn net.Conn) (n int, err error)
+	WriteFrame(b []byte, conn net.Conn) (n int, err error)
 }
 
 type MBLB struct {
@@ -30,12 +30,12 @@ type MBLB struct {
  * 格式:[FRAMEFLAG0][FRAMEFLAG0][length](data........])
  *      前两2个是协议开头标志　　后面数据字节数　　　data为真实数据
  */
-func WriteFrame(b []byte,conn net.Conn) (n int, err error) {
+func WriteFrame(b []byte, conn net.Conn) (n int, err error) {
 	length := len(b)
-	if length > MAX_FRAME_LEN{
+	if length > MAX_FRAME_LEN {
 		return 0, fmt.Errorf("message too long\n")
 	}
-	frameHeader := make([]byte, FRAME_HEADER_LEN + length)
+	frameHeader := make([]byte, FRAME_HEADER_LEN+length)
 	f := NewLEStream(frameHeader)
 	f.WriteUint16(FRAMEFLAG)
 	f.WriteUint16(uint16(len(b)))
@@ -46,9 +46,7 @@ func WriteFrame(b []byte,conn net.Conn) (n int, err error) {
 	return conn.Write(f.buff[:f.pos])
 }
 
-
-
-func ReadFrame(b []byte,conn net.Conn) (n int, err error) {
+func ReadFrame(conn net.Conn) (n int, err error) {
 	//step 1: 分配frame长度的大小的空间
 	frameHeader := make([]byte, FRAME_HEADER_LEN)
 	var n int
@@ -59,6 +57,8 @@ func ReadFrame(b []byte,conn net.Conn) (n int, err error) {
 		n, err = conn.Read(frameHeader[realNeed:])
 		if err != nil && err != io.EOF {
 			return 0, fmt.Errorf("Read Frame error:%s", err)
+		} else if err == io.EOF {
+			return 0, err
 		}
 		realNeed = realNeed + n
 		if realNeed == FRAME_HEADER_LEN || 0 == n {
@@ -77,19 +77,21 @@ func ReadFrame(b []byte,conn net.Conn) (n int, err error) {
 		return FRAME_HEADER_LEN, fmt.Errorf("read frame length is wrong: %s\n", errLength)
 	}
 	if frameFlag != FRAMEFLAG {
-		return FRAME_HEADER_LEN,fmt.Errorf("[ReadPacket]frame flag is wrong:0x%x", frameFlag)
+		return FRAME_HEADER_LEN, fmt.Errorf("frame flag is wrong:0x%x", frameFlag)
 	}
 
 	if frameLength > MAX_FRAME_LEN {
-		return FRAME_HEADER_LEN,fmt.Errorf("[ReadPacket]SecDataLen wrong:0x%x", frameLength)
+		return FRAME_HEADER_LEN, fmt.Errorf("frameLength wrong:0x%x", frameLength)
 	}
-	//step 3: 分配加了密的sec Data的长度的空间
+	//step 3: 读取frame中的数据
 	frameBody := make([]byte, frameLength)
 
 	for {
 		n, err = conn.Read(frameBody[realNeed:])
 		if err != nil && err != io.EOF {
 			return fmt.Errorf("[Readpacket] read Sec Data error:", err)
+		} else if err == io.EOF {
+			return 0, err
 		}
 		realNeed = realNeed + n
 		if realNeed == int(frameLength) || n == 0 {
@@ -98,7 +100,6 @@ func ReadFrame(b []byte,conn net.Conn) (n int, err error) {
 		}
 
 	}
-
 
 	return frameLength, nil
 }
